b3e316a320e68dbcb4e9229d20b024714c41bd8d
masaminh <masami.askw@gmail.com>
2022-06-25 16:19:28 +0900

    Initial commit

---
+++ test/notify_school_v2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NotifySchoolV2 from '../lib/notify_school_v2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/notify_school_v2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NotifySchoolV2.NotifySchoolV2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
